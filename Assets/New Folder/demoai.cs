6d40f740c51db393b3b65f74a6d4c9d1356fb2aa
Josh <joshuamorisonhalliwell@gmail.com>
2021-10-05 15:06:02 +1300

    Ai and Raycast
    Ai with raycast

---
+++ Assets/New Folder/demoai.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class demoai : MonoBehaviour
+{
+    //AI
+    NavMeshAgent agent = null;
+    GameObject TargetPoint = null;
+
+    //Raycast
+    public float raycastDist = 10;
+    public LayerMask mask = 0;
+
+    GameObject HitTarget = null;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        agent = GetComponent<NavMeshAgent>();
+        TargetPoint = new GameObject("Targetpoint");
+        HitTarget = TargetPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 direction = new Vector3(Mathf.Sin(Time.time), 0, 1);
+        //agent.Move(direction * Time.deltaTime * 5);
+
+        //agent.SetDestination(TargetPoint.transform.position);
+        RaycastHit Hit;
+        Debug.DrawRay(transform.position, transform.forward * raycastDist, Color.magenta);
+        if (Physics.Raycast(transform.position, transform.forward, out Hit, raycastDist, mask))
+        {
+            HitTarget = Hit.transform.gameObject;
+            Debug.Log("works");
+        }
+        agent.SetDestination(HitTarget.transform.position);
+    }
+}
